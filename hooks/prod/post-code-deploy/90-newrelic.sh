1cb9634389ca57de2491b3c1e95d2f9ae4475f5b
Patrick Durold <patrick.durold@valiton.com>
2016-08-12 15:00:24 +0200

    - suppress errors - fix forgotten include

--- hooks/prod/post-code-deploy/90-newrelic.sh
+++ hooks/prod/post-code-deploy/90-newrelic.sh
@@ -9,6 +9,10 @@ deployedtag=$4  # The code branch or tag being deployed.
 repourl=$5      # The URL of your code repository.
 repotype=$6     # The version control system your site is using; "git" or "svn".
 
+set +e
+
+. $HOME/newrelic_settings
+
 curl -s -H "x-api-key:$APIKEY" -d "deployment[application_id]=$APPID" -d "deployment[description]=$deployedtag deployed to $site:$targetenv" -d "deployment[revision]=$deployedtag" -d "deployment[changelog]=$deployedtag deployed to $site:$targetenv" -d "deployment[user]=$username"  https://api.newrelic.com/deployments.xml
 # never break deployment
 exit 0
